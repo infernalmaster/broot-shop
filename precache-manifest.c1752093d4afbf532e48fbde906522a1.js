a4e9b86489d254c28002c28ff75f78e45d220b5d
Rostyslav Diachok <infernalmaster1@gmail.com>
2018-12-20 11:38:50 +0200

    Updates

---
+++ precache-manifest.c1752093d4afbf532e48fbde906522a1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7e2dd6a3749a6c4ef815",
+    "url": "/static/js/main.7e2dd6a3.chunk.js"
+  },
+  {
+    "revision": "0b6a0b0b2167397bbe2e",
+    "url": "/static/js/1.0b6a0b0b.chunk.js"
+  },
+  {
+    "revision": "7e2dd6a3749a6c4ef815",
+    "url": "/static/css/main.22feb94e.chunk.css"
+  },
+  {
+    "revision": "9d3de8749caac3fad212126e833961e5",
+    "url": "/index.html"
+  }
+];
